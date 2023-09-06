86fe8a8214c9820bc5431773d7d70fa07544dce9
Monx13 <monx1314@hotmail.com>
2023-09-06 11:56:20 -0600

    Editando NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/Buho.jpeg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import logo from '../assets/img/Buho2.png';
+import github from '../assets/img/github.png';
+import facebook from '../assets/img/facebook.png';
+import instagram from '../assets/img/logotipo-de-instagram.png';
+import discord from '../assets/img/discordia.png';
+import descarga from '../assets/img/descargar.png';
 import { HashLink } from 'react-router-hash-link';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 
 export const NavBar = () => {
 
@@ -46,17 +46,18 @@ export const NavBar = () => {
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+              <Nav.Link href="#studies" className={activeLink === 'studies' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('studies')}>Studies</Nav.Link>
+              <Nav.Link href="#certifications" className={activeLink === 'certifications' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('certifications')}>Certifications</Nav.Link>
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://github.com/Monx13" target="_blank"><img src={github} alt="" /></a>
+                <a href="https://www.facebook.com/vazmonserrat13" target="_blank"><img src={facebook} alt="" /></a>
+                <a href="https://www.instagram.com/_monx13_" target="_blank"><img src={instagram} alt="" /></a>
+                <a href="https://discord.gg/YyVJtMbK" target="_blank"><img src={discord} alt="" /></a>
+                <a href="../assets/documents/CV-Adamari-Lopez.pdf" download><img src={descarga} alt="" /></a>
               </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
